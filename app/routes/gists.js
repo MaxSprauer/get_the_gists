86a738f575a82e1dcb45c580a900cc88b9defcee
Max Sprauer <max.sprauer@quest.com>
2021-10-24 14:47:32 -0400

    Get list and single gist routes working

--- app/routes/gists.js
+++ app/routes/gists.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios').default;
+const ghAxios = axios.create({
+  baseURL: 'https://api.github.com',
+  timeout: 1000,
+  headers: {'Accept': 'application/vnd.github.v3+json'}
+});
+
 
 /* GET a list of gists marked as favorites. */
 // TODO: This prevents a gistId == favorites, but that should be OK for now.
@@ -9,7 +16,15 @@ router.get('/favorites', function(req, res, next) {
 
 /* GET a gist by ID. */
 router.get('/:gistId', function(req, res, next) {
-  res.send('gistId ' + req.params.gistId);
+    ghAxios.get(`gists/${req.params.gistId}`)
+      .then(function (response) {
+        //TODO Again this is just sending back the whole github json blob
+        res.json(response.data);
+      })
+      .catch(function (error) {
+        res.status(500).send({message: 'There was an error: ' + error});
+        console.error(error);
+      });
 });
 
 /* PUT Mark a gist ID as favorited. */
