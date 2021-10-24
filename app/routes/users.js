4124ae71789077326c113a0f14ac3d32a6d6f437
Max Sprauer <max.sprauer@quest.com>
2021-10-24 16:30:50 -0400

    Clean up code

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,24 +6,24 @@ var router = express.Router();
 
 const axios = require('axios').default;
 const ghAxios = axios.create({
-  baseURL: 'https://api.github.com',
-  timeout: 1000,
-  headers: {'Accept': 'application/vnd.github.v3+json'}
+    baseURL: 'https://api.github.com',
+    timeout: 1000,
+    headers: {'Accept': 'application/vnd.github.v3+json'}
 });
 
 /* GET listing of gists for a user name. */
 router.get('/:userName/gists', function(req, res, next) {
-  //TODO: Add pagination or limit?
-  ghAxios.get(`/users/${req.params.userName}/gists`)
-    .then(function (response) {
-      //TODO: This is just throwing the whole github json blob back; we probably want to filter the object to just
-      // return the interesting bits.
-      res.json(response.data);
-    })
-    .catch(function (error) {
-      res.status(500).send({message: 'There was an error: ' + error});
-      console.error(error);
-    });
+    //TODO: Add pagination or limit?
+    ghAxios.get(`/users/${req.params.userName}/gists`)
+      .then((response) => {
+          //TODO: This is just throwing the whole github json blob back; we probably want to filter the object to just
+          // return the interesting bits.
+          res.json(response.data);
+      })
+      .catch((error) => {
+          res.status(500).send({message: 'There was an error: ' + error});
+          console.error(error);
+      });
 });
 
 module.exports = router;
